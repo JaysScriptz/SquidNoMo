a604c0b2467be987716484f4be95e148bf7f3899
JaysScriptz <jasminethatcher1998@gmail.com>
2026-07-17 17:26:42 -0500

    Update notifications and load configuration in Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,31 @@
+--//========================================================
+--// SquidNoMo Beta 5.0
+--// Main.lua
+--// Stage 2
+--//========================================================
+
 print("MAIN STARTED")
 
 pcall(function()
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "SquidNoMo",
-        Text = "Main.lua Started",
-        Duration = 10
-    })
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = "SquidNoMo",
+		Text = "Main Started",
+		Duration = 5
+	})
+end)
+
+local Config = loadstring(game:HttpGet(
+	"https://raw.githubusercontent.com/JaysScriptz/SquidNoMo/main/Config.lua"
+))()
+
+print("CONFIG LOADED")
+
+pcall(function()
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = "SquidNoMo",
+		Text = "Config Loaded",
+		Duration = 5
+	})
 end)
 
 return true
