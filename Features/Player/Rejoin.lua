f2e22ca4b779f7e2806a5f14acfef5b683feaf00
JaysScriptz <jasminethatcher1998@gmail.com>
2026-07-17 19:34:29 -0500

    Add Rejoin functionality for players

---
+++ Features/Player/Rejoin.lua
@@ -0,0 +1,36 @@
+--//========================================================--
+--// SquidNoMo
+--// Beta 5.0
+--// Player
+--// Rejoin.lua
+--//========================================================--
+
+local Rejoin = {}
+
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+
+local LocalPlayer = Players.LocalPlayer
+
+----------------------------------------------------------
+-- Execute
+----------------------------------------------------------
+
+function Rejoin:Execute()
+
+	if not game.PlaceId then
+		return
+	end
+
+	TeleportService:Teleport(
+		game.PlaceId,
+		LocalPlayer
+	)
+
+end
+
+----------------------------------------------------------
+-- Return
+----------------------------------------------------------
+
+return Rejoin
