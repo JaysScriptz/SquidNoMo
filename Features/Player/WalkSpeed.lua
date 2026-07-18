a8c5d700f9d5f24f216e58c51f19d26bd3cb1bf2
JaysScriptz <jasminethatcher1998@gmail.com>
2026-07-17 19:21:01 -0500

    Add WalkSpeed module for player speed control

---
+++ Features/Player/WalkSpeed.lua
@@ -0,0 +1,80 @@
+--//========================================================--
+--// SquidNoMo
+--// Beta 5.0
+--// Player
+--// WalkSpeed.lua
+--//========================================================--
+
+local WalkSpeed = {}
+
+local Players = game:GetService("Players")
+
+local LocalPlayer = Players.LocalPlayer
+
+local DefaultSpeed = 16
+
+local CurrentSpeed = DefaultSpeed
+
+----------------------------------------------------------
+-- Apply
+----------------------------------------------------------
+
+local function Apply()
+
+	local Character = LocalPlayer.Character
+
+	if not Character then
+		return
+	end
+
+	local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+
+	if not Humanoid then
+		return
+	end
+
+	Humanoid.WalkSpeed = CurrentSpeed
+
+end
+
+----------------------------------------------------------
+-- Set Speed
+----------------------------------------------------------
+
+function WalkSpeed:Set(Value)
+
+	CurrentSpeed = Value
+
+	Apply()
+
+end
+
+----------------------------------------------------------
+-- Reset
+----------------------------------------------------------
+
+function WalkSpeed:Reset()
+
+	CurrentSpeed = DefaultSpeed
+
+	Apply()
+
+end
+
+----------------------------------------------------------
+-- Character Respawn
+----------------------------------------------------------
+
+LocalPlayer.CharacterAdded:Connect(function()
+
+	task.wait(0.25)
+
+	Apply()
+
+end)
+
+----------------------------------------------------------
+-- Return
+----------------------------------------------------------
+
+return WalkSpeed
