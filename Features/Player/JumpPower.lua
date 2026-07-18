73c1d4cf657743647ee203f0abc83c77ba9948bf
JaysScriptz <jasminethatcher1998@gmail.com>
2026-07-17 19:21:36 -0500

    Add JumpPower module for player jump management

---
+++ Features/Player/JumpPower.lua
@@ -0,0 +1,80 @@
+--//========================================================--
+--// SquidNoMo
+--// Beta 5.0
+--// Player
+--// JumpPower.lua
+--//========================================================--
+
+local JumpPower = {}
+
+local Players = game:GetService("Players")
+
+local LocalPlayer = Players.LocalPlayer
+
+local DefaultJumpPower = 50
+local CurrentJumpPower = DefaultJumpPower
+
+----------------------------------------------------------
+-- Apply
+----------------------------------------------------------
+
+local function Apply()
+
+	local Character = LocalPlayer.Character
+
+	if not Character then
+		return
+	end
+
+	local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+
+	if not Humanoid then
+		return
+	end
+
+	Humanoid.UseJumpPower = true
+	Humanoid.JumpPower = CurrentJumpPower
+
+end
+
+----------------------------------------------------------
+-- Set Jump Power
+----------------------------------------------------------
+
+function JumpPower:Set(Value)
+
+	CurrentJumpPower = Value
+
+	Apply()
+
+end
+
+----------------------------------------------------------
+-- Reset
+----------------------------------------------------------
+
+function JumpPower:Reset()
+
+	CurrentJumpPower = DefaultJumpPower
+
+	Apply()
+
+end
+
+----------------------------------------------------------
+-- Character Respawn
+----------------------------------------------------------
+
+LocalPlayer.CharacterAdded:Connect(function()
+
+	task.wait(0.25)
+
+	Apply()
+
+end)
+
+----------------------------------------------------------
+-- Return
+----------------------------------------------------------
+
+return JumpPower
