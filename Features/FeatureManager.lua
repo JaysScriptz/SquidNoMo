b6f5fd4873f031c16cdfee79063a7dc8b088716b
JaysScriptz <jasminethatcher1998@gmail.com>
2026-07-17 20:09:20 -0500

    Refactor Player: Use self in Initialize method
    Refactor Player initialization to use self and improve structure.

--- Features/FeatureManager.lua
+++ Features/FeatureManager.lua
@@ -8,51 +8,53 @@
 local Player = {}
 
 ----------------------------------------------------------
--- Load Helper
+-- Initialize
 ----------------------------------------------------------
 
-local function Load(Module)
+function Player:Initialize(Loader)
 
-	return loadstring(game:HttpGet(
+	local function Load(Module)
 
-		App.Config.Repository ..
-		"Features/Player/" ..
-		Module ..
-		".lua"
+		return loadstring(game:HttpGet(
 
-	))()
+			Loader.Config.Repository ..
+			"Features/Player/" ..
+			Module ..
+			".lua"
+
+		))()
+
+	end
+
+	------------------------------------------------------
+	-- Enhancements
+	------------------------------------------------------
+
+	self.WalkSpeed = Load("WalkSpeed")
+	self.JumpPower = Load("JumpPower")
+	self.InfiniteJump = Load("InfiniteJump")
+	self.Noclip = Load("Noclip")
+
+	------------------------------------------------------
+	-- ESP
+	------------------------------------------------------
+
+	self.PlayerESP = Load("PlayerESP")
+	self.GuardESP = Load("GuardESP")
+	self.DetectiveESP = Load("DetectiveESP")
+	self.FrontmanESP = Load("FrontmanESP")
+
+	------------------------------------------------------
+	-- Utilities
+	------------------------------------------------------
+
+	self.AntiAFK = Load("AntiAFK")
+	self.AntiLag = Load("AntiLag")
+	self.Reset = Load("Reset")
+	self.Rejoin = Load("Rejoin")
+
+	return self
 
 end
-
-----------------------------------------------------------
--- Enhancements
-----------------------------------------------------------
-
-Player.WalkSpeed     = Load("WalkSpeed")
-Player.JumpPower     = Load("JumpPower")
-Player.InfiniteJump  = Load("InfiniteJump")
-Player.Noclip        = Load("Noclip")
-
-----------------------------------------------------------
--- ESP
-----------------------------------------------------------
-
-Player.PlayerESP     = Load("PlayerESP")
-Player.GuardESP      = Load("GuardESP")
-Player.DetectiveESP  = Load("DetectiveESP")
-Player.FrontmanESP   = Load("FrontmanESP")
-
-----------------------------------------------------------
--- Utilities
-----------------------------------------------------------
-
-Player.AntiAFK       = Load("AntiAFK")
-Player.AntiLag       = Load("AntiLag")
-Player.Reset         = Load("Reset")
-Player.Rejoin        = Load("Rejoin")
-
-----------------------------------------------------------
--- Return
-----------------------------------------------------------
 
 return Player
